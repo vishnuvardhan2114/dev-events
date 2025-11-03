e73d71b2ab8e70df9f683add9bb35645228821bd
vishnuvardhan2114 <vishnuvardhan14102002@gmail.com>
2025-11-03 17:36:52 +0530

    Refactor event fetching in main page component
    - Commented out the API call for fetching events to streamline data handling.
    - Imported events from a constants file for better management of event data.
    - Updated event mapping to remove TypeScript type annotation for improved flexibility.

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,14 @@ import EventCard from "@/components/EventCard"
 import ExploreBtn from "@/components/ExploreBtn"
 import { cacheLife } from "next/cache";
 import { EventDocument } from "@/database";
+import events from "@/lib/constants";
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const page = async () => {
   'use cache';
   cacheLife('hours')
-  const response = await fetch(`${BASE_URL}/api/events`);
-  const { events } = await response.json();
+  // const response = await fetch(`${BASE_URL}/api/events`);
+  // const { events } = await response.json();
   return (
     <section>
       <h1 className="text-center">The Hub for Every Dev <br /> Event You Can&apos;t Miss</h1>
@@ -18,7 +19,7 @@ const page = async () => {
         <h3>Featured Events</h3>
 
         <ul className="events">
-          {events.map((event: EventDocument) => (
+          {events.map((event) => (
             <li key={event.title} className="list-none">
               <EventCard {...event} />
             </li>
