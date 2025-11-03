9a74038487d0301e4b0a87542a565db96c7e4fef
vishnuvardhan2114 <vishnuvardhan14102002@gmail.com>
2025-11-03 12:43:27 +0530

    Add PostHog integration and event management features
    - Introduced `instrumentation-client.ts` for PostHog analytics setup.
    - Updated `next.config.ts` to include API rewrites for PostHog.
    - Added new components: `EventCard`, `EventDetails`, `ExploreBtn`, and `LightRays` for event display and interaction.
    - Created a constants file for event data and updated the main page layout.
    - Enhanced global styles with Tailwind CSS and added utility classes.
    - Implemented a responsive navigation bar and event detail pages.
    - Included various SVG icons and images for improved UI.
    - Removed unused SVG files to clean up the project structure.

---
+++ app/events/[slug]/page.tsx
@@ -0,0 +1,15 @@
+import EventDetails from "@/components/EventDetails";
+import { Suspense } from "react";
+
+const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }>}) => {
+    const { slug } = await params;
+    return (
+        <main>
+            <Suspense fallback={<div>Loading...</div>}>
+                <EventDetails slug={slug} />
+            </Suspense>
+        </main>
+    )
+}
+
+export default EventDetailsPage
---
+++ components/EventDetails.tsx
@@ -0,0 +1,9 @@
+const EventDetails = ({ slug }: { slug: string }) => {
+  return (
+    <div>
+        <h1>{slug}</h1>
+    </div>
+  )
+}
+
+export default EventDetails
